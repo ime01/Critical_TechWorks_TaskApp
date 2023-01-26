d485de5d7eb1845e6963d2b78edb2b1ec0bf964a
imevbore <ketuimevbore@yahoo.com>
2023-01-26 16:51:15 +0100

    splash screen with company logo, News and Details Fragment with Jetpack navigation setup

--- app/src/main/java/com/example/criticaltechworkstaskapp/MainActivity.kt
+++ app/src/main/java/com/example/criticaltechworkstaskapp/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.criticaltechworkstaskapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.criticaltechworkstaskapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setTheme(R.style.Theme_CriticalTechWorksTaskApp)
+        setContentView(binding.root)
+
+
+        //API KEY
+        //6496c837bca04576875ffbe8115eb2f5
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+
+        return navController.navigateUp()|| super.onSupportNavigateUp()
     }
 }
