cae0c306127a2a27cd626f4257278312de67539b
imevbore <ketuimevbore@yahoo.com>
2023-01-28 01:44:55 +0100

    3 other Flavours Added with Main Flavour, making 4 buildable product flavours of this app

--- app/src/main/java/com/example/criticaltechworkstaskapp/common/Constants.kt
+++ app/src/main/java/com/example/criticaltechworkstaskapp/common/Constants.kt
@@ -3,7 +3,7 @@ package com.example.criticaltechworkstaskapp.common
 object Constants {
     const val BASE_URL = "https://newsapi.org/v2/"
     const val MAIN_APP_NEWS_SOURCE = "bbc-news"
-    const val OTHER_FLAVOUR1_NEWS_SOURCE = "reuters"
-    const val OTHER_FLAVOUR2_NEWS_SOURCE = "google-news"
-    const val OTHER_FLAVOUR3_NEWS_SOURCE = "cnn"
+    const val REUTERS_FLAVOUR_NEWS_SOURCE = "reuters"
+    const val GOOGLE_FLAVOUR_NEWS_SOURCE = "google-news"
+    const val CNN_FLAVOUR_NEWS_SOURCE = "cnn"
 }
--- app/src/main/java/com/example/criticaltechworkstaskapp/data/repository/NewsRepositoryImpl.kt
+++ app/src/main/java/com/example/criticaltechworkstaskapp/data/repository/NewsRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.example.criticaltechworkstaskapp.data.repository
 
 
 import com.example.criticaltechworkstaskapp.BuildConfig
+import com.example.criticaltechworkstaskapp.common.Constants.CNN_FLAVOUR_NEWS_SOURCE
 import com.example.criticaltechworkstaskapp.common.Constants.MAIN_APP_NEWS_SOURCE
 import com.example.criticaltechworkstaskapp.data.remote.NewsApi
 import com.example.criticaltechworkstaskapp.data.remote.dto.NewsResponse
@@ -11,6 +12,6 @@ import javax.inject.Inject
 class NewsRepositoryImpl @Inject constructor (private val api: NewsApi): NewsRepository {
 
     override suspend fun getNews(): NewsResponse {
-       return api.getNewsHeadlines(MAIN_APP_NEWS_SOURCE, BuildConfig.API_KEY)
+       return api.getNewsHeadlines(CNN_FLAVOUR_NEWS_SOURCE, BuildConfig.API_KEY)
     }
 }
