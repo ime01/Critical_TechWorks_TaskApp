a9ec503c76f796d790dce17fb41ce6fe8c770cfb
imevbore <ketuimevbore@yahoo.com>
2023-01-30 00:02:49 +0100

    Instrumentation tests details screen confirmation that data sent their is displayed correctly to the user

--- app/src/androidTest/java/com/example/criticaltechworkstaskapp/ui/fragments/HeadLineDetailsFragmentTest.kt
+++ app/src/androidTest/java/com/example/criticaltechworkstaskapp/ui/fragments/HeadLineDetailsFragmentTest.kt
@@ -1,6 +1,7 @@
 package com.example.criticaltechworkstaskapp.ui.fragments
 
 import android.os.Bundle
+import androidx.core.os.bundleOf
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
@@ -10,6 +11,7 @@ import org.junit.Test
 import com.example.criticaltechworkstaskapp.R
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import com.example.criticaltechworkstaskapp.common.formatDateTime
 import com.example.criticaltechworkstaskapp.domian.model.News
 import com.example.criticaltechworkstaskapp.domian.model.Source
 import org.junit.runner.RunWith
@@ -19,24 +21,43 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 internal class HeadLineDetailsFragmentTest{
 
+    val news = News(
+        author = "JOHN WICK",
+        content = "JOHN WICK SPOTTED IS BACK FIGHTING HIS WAY OUT",
+        description = "JOHN WICK SPOTTED",
+        publishedAt = "29/01/2023",
+        source = Source("cnn", "CNN News"),
+        title = "JOHN WICK",
+        urlToImage = ""
+    )
+
 
     @Before
     fun setUp() {
 
-        launchFragmentInContainer<HeadLineDetailsFragment>(fragmentArgs = null, factory = null)
+        launchFragmentInContainer<HeadLineDetailsFragment>(fragmentArgs =  bundleOf(
+            "newsModel" to  news,
+        ), factory = null)
     }
 
     @Test
-    fun confirmViewsWithImageTitleDesandContentAreDisplayed() {
+    fun confirmViewsCorrectNewsDataPassedtoDetailsScreenIsDisplayed() {
 
-      onView(withId(R.id.image_detail_large))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-       onView(withId(R.id.dtitle))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-       onView(withId(R.id.ddescription))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        onView(withId(R.id.dcontent))
-            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        onView(withId(R.id.dtitle)).check(ViewAssertions.matches(
+            ViewMatchers.withText(news.title)
+        ))
+
+        onView(withId(R.id.ddescription)).check(ViewAssertions.matches(
+            ViewMatchers.withText(news.description)
+        ))
+
+        onView(withId(R.id.dcontent)).check(ViewAssertions.matches(
+            ViewMatchers.withText(news.content)
+        ))
+
+        onView(withId(R.id.ddate)).check(ViewAssertions.matches(
+            ViewMatchers.withText(formatDateTime(news.publishedAt))
+        ))
 
     }
 
